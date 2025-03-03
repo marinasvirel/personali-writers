1d70732994bc1592a243090a4fd275d7ee6d2ca5
marinasvirel <marusenka_83@mail.ru>
2025-03-03 05:00:50 +0300

    validation

--- functions.php
+++ functions.php
@@ -9,16 +9,48 @@ function validation($pattern, $condition, $post)
       if (preg_match($pattern, $input) == 0) {
         $result = $condition;
       } else {
-        $result = "ok";
+        $result = "<span class='info-span-valid'>&#10004;</span>";
       }
     } else {
-      $result = "пустая строка";
+      $result = "Заполните поле";
     }
   }
   return $result;
 }
 
-function validName($name)
+function validName($post)
 {
-  return validation("#^[А-ЯЁ][а-яё][а-яёА-ЯЁ\- ]*$#u", "Только буквы кириллические. Без пробелов. Строка начинается с прописной буквы. Не менее двух символов", $name);
+  return validation("#^[А-ЯЁ][а-яё][а-яёА-ЯЁ\- ]*$#u", "Только буквы кириллические. Строка начинается с прописной буквы. Не менее двух символов. Допускаются пробелы и дефисы", $post);
+}
+
+function validBiograhy($post)
+{
+  return validation('#^[А-ЯЁ][а-яё][а-яёА-ЯЁ\-\?\!\.\,\"\:\; ]*$#u', "Только буквы кириллические. Строка начинается с прописной буквы. Не менее двух символов. Допускаются пробелы, дефисы, кавычки, точки, запятые, двоеточия, точки с запятой, восклицательный и вопросительный знак", $post);
+}
+
+function validDate($post)
+{
+  $result = "";
+  if (isset($_POST[$post])) {
+    $input = $_POST[$post];
+    if (empty($input)) {
+      $result = "Выберите дату";
+    } else {
+      $result = "<span class='info-span-valid'>&#10004;</span>";
+    }
+  }
+  return $result;
+}
+
+function validFile()
+{
+  $result = "";
+  if (isset($_FILES['file'])) {
+    if (empty($_FILES['file']['name'])) {
+      $result = "Загрузите изображение";
+    } else {
+      $result = "<span class='info-span-valid'>&#10004;</span>";
+    }
+  }
+  return $result;
 }
--- info.php
+++ info.php
@@ -1,50 +1,66 @@
 <?php
-
 require_once "functions.php";
+
+function validWork(){}
 
 $name_span = validName('name');
 $surname_span = validName('surname');
+$patronymic_span = validName('patronymic');
+$date_birth_span = validDate("date-birth");
+$date_died_span = validDate("date-died");
+$file_span = validFile();
+$biography_span = validBiograhy("biography");
 
-
-//var_dump();
-var_dump($_POST)
+// var_dump();
 ?>
+<h2>Информация о писателе</h2>
 <form class="info" action="" method="post" enctype="multipart/form-data">
   <span class="info-span"><?= $name_span ?></span>
   <input type="text" name="name" placeholder="Имя">
   <span class="info-span"><?= $surname_span ?></span>
   <input type="text" name="surname" placeholder="Фамилия">
+  <span class="info-span"><?= $patronymic_span ?></span>
   <input type="text" name="patronymic" placeholder="Отчество">
-  <input type="text" name="date-birth" placeholder="Дата рождения дд.мм.гггг">
-  <input type="text" name="date-died" placeholder="Дата смерти дд.мм.гггг">
+  <span class="info-span"><?= $date_birth_span ?></span>
+  <div class="date-wrapper">
+    <p>Дата&nbsp;рождения:</p>
+    <input type="date" name="date-birth" id="date-birth">
+  </div>
+  <span class="info-span"><?= $date_died_span ?></span>
+  <div class="date-wrapper">
+    <p>Дата&nbsp;смерти:</p>
+    <input type="date" name="date-died" id="">
+  </div>
+  <span class="info-span"><?= $file_span ?></span>
   <div class="file-container">
-    <input type="file" id="file">
+    <input type="file" id="file" name="file">
     <label for="file" id="for-file">Изображение писателя</label>
   </div>
+  <span class="info-span"><?= $biography_span ?></span>
   <textarea name="biography" id="" placeholder="Биография"></textarea>
-  <div id="fieldList">
-    <div class="field-container">
-      <input type="text" name="field[]" placeholder="Произведение">
+  <div id="workList">
+    <div class="work-container">
+      <input type="text" name="works[]" placeholder="Произведение" required>
     </div>
   </div>
-  <button type="button" id="addFieldBtn">Добавить произведение</button>
+  <button type="button" id="addWorkBtn">Добавить произведение</button>
   <button type="submit">Опубликовать</button>
 </form>
 
 <script>
   // Добавление полей
-  document.getElementById('addFieldBtn').addEventListener('click', function() {
-    const fieldList = document.getElementById('fieldList');
-    const newFieldContainer = document.createElement('div');
-    newFieldContainer.className = 'field-container';
-    newFieldContainer.innerHTML = `
-            <input type="text" name="field[]" placeholder="Произведение" required>
+  document.getElementById('addWorkBtn').addEventListener('click', function() {
+    const workList = document.getElementById('workList');
+    const newWorkContainer = document.createElement('div');
+    newWorkContainer.className = 'work-container';
+    newWorkContainer.innerHTML = `
+            <input type="text" name="works[]" placeholder="Произведение" required>
             <button type="button" class="remove-btn"></button>
         `;
-    fieldList.appendChild(newFieldContainer);
+    workList.appendChild(newWorkContainer);
   });
 
-  document.getElementById('fieldList').addEventListener('click', function(e) {
+  document.getElementById('workList').addEventListener('click', function(e) {
     if (e.target.classList.contains('remove-btn')) {
       e.target.parentElement.remove();
     }
